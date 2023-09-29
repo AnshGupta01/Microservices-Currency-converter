8bd83994848b4d3dbc8274c8b68f9d706e4fc548
Ansh <26479077+AnshGupta01@users.noreply.github.com>
2023-09-29 20:25:12 +0530

    JPA added to CurrencyExchange.java

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,19 +1,24 @@
 package com.microservices.currencyexchangeservice;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.math.BigDecimal;
 
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class CurrencyExchange {
 
+    @Id
     private long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
