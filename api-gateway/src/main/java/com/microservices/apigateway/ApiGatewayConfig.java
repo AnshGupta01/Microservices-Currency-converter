37996c30f3bb5a55736448363ac5981cfa92c2e9
Ansh <26479077+AnshGupta01@users.noreply.github.com>
2023-09-30 17:40:15 +0530

    Docker-compose set up & pom.xml modified for creation of image using "mvn spring-boot:build-image -DskipTests" command

--- api-gateway/src/main/java/com/microservices/apigateway/ApiGatewayConfig.java
+++ api-gateway/src/main/java/com/microservices/apigateway/ApiGatewayConfig.java
@@ -20,8 +20,8 @@ public class ApiGatewayConfig {
                 .route(p -> p.path("/currency-exchange/**")
                         .uri("lb://currency-exchange"))
 
-                .route(p -> p.path("/currency-conversion/**")
-                        .uri("lb://currency-conversion"))
+                //.route(p -> p.path("/currency-conversion/**")
+                //        .uri("lb://currency-conversion"))
 
                 .route(p -> p.path("/currency-conversion-feign/**")
                         .uri("lb://currency-conversion"))
--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -32,26 +32,26 @@ public class CurrencyConversionController {
     }
 
 
-    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calcualateCurrencyConversion(@PathVariable String from,
-                                                           @PathVariable String to,
-                                                           @PathVariable BigDecimal quantity) {
-
-        HashMap<String,String> uriVariables = new HashMap<>();
-        uriVariables.put("from",from);
-        uriVariables.put("to",to);
-
-        ResponseEntity<CurrencyConversion> responseEntity =
-                restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-                CurrencyConversion.class, uriVariables);
-
-        CurrencyConversion currencyConversion = responseEntity.getBody();
-        return new CurrencyConversion(currencyConversion.getId(),
-                from,to,quantity,
-                currencyConversion.getConversionMultiple(),
-                quantity.multiply(currencyConversion.getConversionMultiple()),
-                currencyConversion.getEnvironment());
-    }
+//    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+//    public CurrencyConversion calcualateCurrencyConversion(@PathVariable String from,
+//                                                           @PathVariable String to,
+//                                                           @PathVariable BigDecimal quantity) {
+//
+//        HashMap<String,String> uriVariables = new HashMap<>();
+//        uriVariables.put("from",from);
+//        uriVariables.put("to",to);
+//
+//        ResponseEntity<CurrencyConversion> responseEntity =
+//                restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+//                CurrencyConversion.class, uriVariables);
+//
+//        CurrencyConversion currencyConversion = responseEntity.getBody();
+//        return new CurrencyConversion(currencyConversion.getId(),
+//                from,to,quantity,
+//                currencyConversion.getConversionMultiple(),
+//                quantity.multiply(currencyConversion.getConversionMultiple()),
+//                currencyConversion.getEnvironment());
+//    }
 
     @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calcualateCurrencyConversionFeign(@PathVariable String from,
